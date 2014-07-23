b63614f091a95995b50bd872f23721e4abc16fae
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-07-23 11:42:13 +0200

    ZOMFG TABLE WITHOUT PRIMARY KEY!!!!

--- sql/install.sql
+++ sql/install.sql
@@ -2,5 +2,6 @@ CREATE TABLE `resources_rooms_order` (
   `resource_id` varchar(32) NOT NULL,
   `priority` int(5) NOT NULL,
   `checked` int(1) NOT NULL,
-  `user_id` varchar(32) NOT NULL
+  `user_id` varchar(32) NOT NULL,
+  PRIMARY KEY (`resource_id`,`user_id`)
 ) 
