a3af08c76635f68661ff060b34f1b6f4d01aff67
Thomas Hackl <thomas.hackl@uni-passau.de>
2018-10-16 14:14:30 +0200

    make rooms sortable

--- views/index/settings_tree.php
+++ views/index/settings_tree.php
@@ -2,7 +2,7 @@
     <label for="<?= $resource->id ?>"><?= htmlReady($resource->name) ?></label>
     <input type="hidden" name="resources[<?= $resource->id ?>]" value="0">
     <input name="resources[<?= $resource->id ?>]" id="<?= $resource->id ?>" type="checkbox" value="1" <?= $resource->checked ?>>
-    <ul class="sortable">
+    <ul class="can-be-sorted">
         <? foreach ($resource->children->orderBy('priority') as $resource): ?>
             <?= $this->render_partial('index/settings_tree', array('resource' => $resource)) ?>
         <? endforeach; ?>
